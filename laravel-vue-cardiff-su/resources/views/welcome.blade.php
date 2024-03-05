3574086e72ba93cc148d212459c4cec4c0fc3caf
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2024-03-05 02:14:59 +0000

    compiling css

--- laravel-vue-cardiff-su/resources/views/welcome.blade.php
+++ laravel-vue-cardiff-su/resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <!-- Import the Vite JavaScript bundle --> 
-    @vite('resources/js/app.js')
+    <!-- Import the Vite JavaScript and css bundle --> 
+    @vite(['resources/js/app.js','resources/css/app.css'])
 </head>
 <body>
     <!-- Mount the Vue application-->
